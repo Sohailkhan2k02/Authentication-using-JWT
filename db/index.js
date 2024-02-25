a4b5040d5ad4fd9f5203ac4d40a46f9b6cd539a5
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-02-25 19:05:09 +0530

    Add files via upload

---
+++ db/index.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://sksohail339:Sohailkhan12@cluster0.dsarucm.mongodb.net/');
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+    // Schema definition here
+    username:String,
+    password:String
+});
+
+const UserSchema = new mongoose.Schema({
+    // Schema definition here
+    username:String,
+    password:String,
+    purchasedCourses: [{
+        type:mongoose.Schema.Types.ObjectId, 
+        ref:'Course'
+    }]
+});
+
+const CourseSchema = new mongoose.Schema({
+    // Schema definition here
+    title:String,
+    description:String,
+    price:Number,
+    imagelink:String,
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
